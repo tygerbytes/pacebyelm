04db04447b6f242e08c68c62b5f2b6ade046abc5
Ty Walls <tygerbytes@users.noreply.github.com>
2017-03-14 18:01:10 -0400

    Tidy up formatting

--- pace.elm
+++ pace.elm
@@ -6,13 +6,12 @@ import Html.Events exposing (onClick)
 import Http
 import Json.Decode as Decode exposing (Decoder, field, succeed)
 import Json.Encode as Encode
-
 import TargetPace
 import RunType
 
 
-
 -- MODEL
+
 
 type alias Model =
     { fiveKTime : String
@@ -22,9 +21,8 @@ type alias Model =
 
 initialModel : Model
 initialModel =
-    {
-      fiveKTime = "25:00"
-    , toggles = initialToggles 
+    { fiveKTime = "25:00"
+    , toggles = initialToggles
     }
 
 
@@ -35,20 +33,22 @@ type alias StatsToggle =
     , activated : Bool
     , glyphicon : String
     , text : String
-    } 
+    }
 
 
 initialToggles : List StatsToggle
 initialToggles =
-    [ StatsToggle "toggle_default" "default" True  True  "glyphicon-heart-empty" "Default"
-    , StatsToggle "toggle_units"   "units"   False False "glyphicon-wrench"      "Units"
+    [ StatsToggle "toggle_default" "default" True True "glyphicon-heart-empty" "Default"
+    , StatsToggle "toggle_units" "units" False False "glyphicon-wrench" "Units"
     ]
 
 
 
 -- UPDATE
 
-type Msg = LoadRunnerStats
+
+type Msg
+    = LoadRunnerStats
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -61,56 +61,62 @@ update msg model =
 
 -- COMMANDS
 
+
 loadRunnerStats : Cmd Msg
 loadRunnerStats =
     Cmd.none
 
 
+
 -- VIEW
+
 
 viewHeader : Html Msg
 viewHeader =
     div [ class "header clearfix" ]
-      [ nav []
-          [ ul [ class "nav nav-pills pull-right" ]
-              [ li [ class "active", attribute "role" "presentation" ]
-                  [ a [ href "#" ]
-                      [ text "Home" ]
-                  ]
-              ]
-          ]
-      , h3 [ class "text-muted" ]
-          [ text "🏃 Runby Pace, by "
-          , a [ href "https://tygertec.com" ]
-              [ text "tygertec" ]
-          ]
-      ]
+        [ nav []
+            [ ul [ class "nav nav-pills pull-right" ]
+                [ li [ class "active", attribute "role" "presentation" ]
+                    [ a [ href "#" ]
+                        [ text "Home" ]
+                    ]
+                ]
+            ]
+        , h3 [ class "text-muted" ]
+            [ text "🏃 Runby Pace, by "
+            , a [ href "https://tygertec.com" ]
+                [ text "tygertec" ]
+            ]
+        ]
 
 
 viewJumbotron : Html Msg
 viewJumbotron =
     div [ class "jumbotron" ]
-      [ h1 []
-          [ text "Training pace calculator, for "
-          , em []
-              [ text "you" ]
-          ]
-      , p [ class "lead" ]
-          [ text "A good running program should incorporate runs at various paces and distances. But how fast should "
-          , em []
-              [ text "you" ]
-          , text " be running for each pace, whether you're on the treadmill or gliding nimbly down the sylvan trail? Well say no more; welcome to the best personalized training pace calculator in the world! – Runby Pace."
-          ]
-      ]
+        [ h1 []
+            [ text "Training pace calculator, for "
+            , em []
+                [ text "you" ]
+            ]
+        , p [ class "lead" ]
+            [ text "A good running program should incorporate runs at various paces and distances. But how fast should "
+            , em []
+                [ text "you" ]
+            , text " be running for each pace, whether you're on the treadmill or gliding nimbly down the sylvan trail? Well say no more; welcome to the best personalized training pace calculator in the world! – Runby Pace."
+            ]
+        ]
 
 
 viewStatsToggle : StatsToggle -> Html Msg
 viewStatsToggle toggle =
-    a [ classList [ ("btn", True)
-                  , ("btn-default", True)
-                  , ("active", toggle.permanent || toggle.activated)
-                  , ("disabled", toggle.permanent) ] 
-      ]
+    a
+        [ classList
+            [ ( "btn", True )
+            , ( "btn-default", True )
+            , ( "active", toggle.permanent || toggle.activated )
+            , ( "disabled", toggle.permanent )
+            ]
+        ]
         [ input [ type_ "checkbox" ]
             []
         , span [ class ("glyphicon " ++ toggle.glyphicon) ]
@@ -127,13 +133,13 @@ viewToggleButtons toggles =
     in
         div [ class "form-group" ]
             [ div [ class "btn-group btn-group-md", attribute "data-toggle" "buttons", id "fieldToggles" ]
-              toggleButtons
+                toggleButtons
             ]
 
 
 viewStatsForm : Html Msg
 viewStatsForm =
-      div [ id "calc_pace" ]
+    div [ id "calc_pace" ]
         [ Html.form [ attribute "accept-charset" "UTF-8", action "/target_pace/calc", attribute "data-remote" "true", method "post" ]
             [ input [ name "utf8", type_ "hidden", value "✓" ]
                 []
@@ -181,17 +187,17 @@ viewStatsForm =
 
 viewFooter : Html Msg
 viewFooter =
-  footer [ class "footer" ]
-    [ div [ class "container" ]
-        [ hr []
-            []
-        , p [ class "text-muted" ]
-            [ text "Runby Pace © 2017 Ty Walls "
-            , a [ href "https://twitter.com/tygertec" ]
-                [ text "@tygertec" ]
+    footer [ class "footer" ]
+        [ div [ class "container" ]
+            [ hr []
+                []
+            , p [ class "text-muted" ]
+                [ text "Runby Pace © 2017 Ty Walls "
+                , a [ href "https://twitter.com/tygertec" ]
+                    [ text "@tygertec" ]
+                ]
             ]
         ]
-    ]
 
 
 view : Model -> Html Msg
@@ -210,13 +216,14 @@ view model =
                 [ TargetPace.viewTargetPacePanel ]
             ]
         , viewFooter
-        ] 
+        ]
 
 
 main : Program Never Model Msg
-main = Html.program
-    { init = ( initialModel, loadRunnerStats )
-    , view = view
-    , update = update
-    , subscriptions = (\_ -> Sub.none )
-    }
+main =
+    Html.program
+        { init = ( initialModel, loadRunnerStats )
+        , view = view
+        , update = update
+        , subscriptions = (\_ -> Sub.none)
+        }
